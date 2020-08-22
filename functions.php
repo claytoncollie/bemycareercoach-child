5371f720d702580af34179cc83949b32229bce0b
Clayton Collie <clayton.collie@gmail.com>
2020-08-22 18:40:51 -0500

    initial commit

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Functions
+ * 
+ * @since 1.0.0
+ */
+
+add_action( 'wp_enqueue_scripts', 'twentysixteen_child_enqueue_assets' );
+/**
+ * Enqueue Assets
+ */
+function twentysixteen_child_enqueue_assets() {
+	wp_enqueue_style('parent-css', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style('child-css', get_stylesheet_uri());
+}
