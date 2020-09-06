0edd45bca673e1802b0080d1c4c956dcb6d117a2
Clayton Collie <clayton.collie@gmail.com>
2020-09-06 11:49:09 -0500

    add version number to resources. Remove entry-footer from archive template to match single

--- functions.php
+++ functions.php
@@ -10,6 +10,6 @@ add_action( 'wp_enqueue_scripts', 'twentysixteen_child_enqueue_assets' );
  * Enqueue Assets
  */
 function twentysixteen_child_enqueue_assets() {
-	wp_enqueue_style('parent-css', get_template_directory_uri() . '/style.css' );
-	wp_enqueue_style('child-css', get_stylesheet_uri());
+	wp_enqueue_style( 'parent-css', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'child-css', get_stylesheet_uri(), array(), '1.0.1' );
 }
