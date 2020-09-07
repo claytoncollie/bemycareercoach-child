e293b927d8a98e11cb66a2e3e850578d469f6e74
Clayton Collie <clayton.collie@gmail.com>
2020-09-07 09:36:37 -0500

    Exclude Page post type from Algolia index

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Functions
- * 
+ *
  * @since 1.0.0
  */
 
@@ -10,6 +10,24 @@ add_action( 'wp_enqueue_scripts', 'twentysixteen_child_enqueue_assets' );
  * Enqueue Assets
  */
 function twentysixteen_child_enqueue_assets() {
-	wp_enqueue_style( 'parent-css', get_template_directory_uri() . '/style.css' );
+	 wp_enqueue_style( 'parent-css', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'child-css', get_stylesheet_uri(), array(), '1.0.2' );
+}
+
+add_filter( 'algolia_should_index_searchable_post', 'twentysixteen_child_exclude_post_types', 10, 2 );
+/**
+ * @param bool    $should_index
+ * @param WP_Post $post
+ *
+ * @return bool
+ */
+function twentysixteen_child_exclude_post_types( $should_index, $post ) {
+	// Add all post types you don't want to make searchable.
+	$excluded_post_types = array( 'page' );
+
+	if ( false === $should_index ) {
+		return false;
+	}
+
+	return ! in_array( $post->post_type, $excluded_post_types, true );
 }
--- header.php
+++ header.php
@@ -4,9 +4,9 @@
  *
  * Displays all of the head element and everything up until the "site-content" div.
  *
- * @package WordPress
+ * @package    WordPress
  * @subpackage Twenty_Sixteen
- * @since Twenty Sixteen 1.0
+ * @since      Twenty Sixteen 1.0
  */
 
 ?><!DOCTYPE html>
@@ -50,50 +50,50 @@
 					<button id="menu-toggle" class="menu-toggle"><?php _e( 'Menu', 'twentysixteen' ); ?></button>
 
 					<div id="site-header-menu" class="site-header-menu">
-						<?php if ( has_nav_menu( 'primary' ) ) : ?>
+					<?php if ( has_nav_menu( 'primary' ) ) : ?>
 							<nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Primary Menu', 'twentysixteen' ); ?>">
-								<?php
-									wp_nav_menu(
-										array(
-											'theme_location' => 'primary',
-											'menu_class' => 'primary-menu',
-										)
-									);
-								?>
+						<?php
+						wp_nav_menu(
+							array(
+								'theme_location' => 'primary',
+								'menu_class'     => 'primary-menu',
+							)
+						);
+						?>
 							</nav><!-- .main-navigation -->
-						<?php endif; ?>
+					<?php endif; ?>
 
-						<?php if ( has_nav_menu( 'social' ) ) : ?>
+					<?php if ( has_nav_menu( 'social' ) ) : ?>
 							<nav id="social-navigation" class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Social Links Menu', 'twentysixteen' ); ?>">
-								<?php
-									wp_nav_menu(
-										array(
-											'theme_location' => 'social',
-											'menu_class'  => 'social-links-menu',
-											'depth'       => 1,
-											'link_before' => '<span class="screen-reader-text">',
-											'link_after'  => '</span>',
-										)
-									);
-								?>
+						<?php
+						wp_nav_menu(
+							array(
+								'theme_location' => 'social',
+								'menu_class'     => 'social-links-menu',
+								'depth'          => 1,
+								'link_before'    => '<span class="screen-reader-text">',
+								'link_after'     => '</span>',
+							)
+						);
+						?>
 							</nav><!-- .social-navigation -->
-						<?php endif; ?>
+					<?php endif; ?>
 					</div><!-- .site-header-menu -->
 				<?php endif; ?>
 			</div><!-- .site-header-main -->
 
 			<?php if ( get_header_image() ) : ?>
 				<?php
-					/**
-					 * Filter the default twentysixteen custom header sizes attribute.
-					 *
-					 * @since Twenty Sixteen 1.0
-					 *
-					 * @param string $custom_header_sizes sizes attribute
-					 * for Custom Header. Default '(max-width: 709px) 85vw,
-					 * (max-width: 909px) 81vw, (max-width: 1362px) 88vw, 1200px'.
-					 */
-					$custom_header_sizes = apply_filters( 'twentysixteen_custom_header_sizes', '(max-width: 709px) 85vw, (max-width: 909px) 81vw, (max-width: 1362px) 88vw, 1200px' );
+				/**
+				 * Filter the default twentysixteen custom header sizes attribute.
+				 *
+				 * @since Twenty Sixteen 1.0
+				 *
+				 * @param string $custom_header_sizes sizes attribute
+				 * for Custom Header. Default '(max-width: 709px) 85vw,
+				 * (max-width: 909px) 81vw, (max-width: 1362px) 88vw, 1200px'.
+				 */
+				$custom_header_sizes = apply_filters( 'twentysixteen_custom_header_sizes', '(max-width: 709px) 85vw, (max-width: 909px) 81vw, (max-width: 1362px) 88vw, 1200px' );
 				?>
 				<div class="header-image">
 					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
