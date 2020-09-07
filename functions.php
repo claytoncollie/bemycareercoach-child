c534c825afeccea94babbdacec70d7edd05ca278
Clayton Collie <clayton.collie@gmail.com>
2020-09-07 09:17:28 -0500

    Remove tempalte-parts. Add CSS from Customizer.

--- functions.php
+++ functions.php
@@ -11,5 +11,5 @@ add_action( 'wp_enqueue_scripts', 'twentysixteen_child_enqueue_assets' );
  */
 function twentysixteen_child_enqueue_assets() {
 	wp_enqueue_style( 'parent-css', get_template_directory_uri() . '/style.css' );
-	wp_enqueue_style( 'child-css', get_stylesheet_uri(), array(), '1.0.1' );
+	wp_enqueue_style( 'child-css', get_stylesheet_uri(), array(), '1.0.2' );
 }
